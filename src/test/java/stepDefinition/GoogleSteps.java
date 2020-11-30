2ae35b0686b8ca6d8f0cd5f60918d51527e6f807
Maria Munteanu <gigelgutu.a@gmail.com>
2020-11-30 16:54:32 +0200

    Adding Laboratory 7

--- src/test/java/stepDefinition/GoogleSteps.java
+++ src/test/java/stepDefinition/GoogleSteps.java
@@ -18,46 +18,18 @@ public class GoogleSteps {
     public static final String AUTOMATE_KEY = "k1mfpX7q4uhCMFXa6RNi";
     public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
     private final By searchField = By.xpath("//input[@name='q']");
-    private final By first = By.xpath("//p[@style='margin-top:1em']");
-    private final By second = By.xpath("//p[@class='p64x9c card-section KDCVqf']");
+    private final By first = By.xpath("//*[@id='topstuff']/div/div");
+    private final By second = By.xpath("//*[@id='tsf']/div[2]/div[1]/div[2]/div/div[2]/input");
     WebDriver driver;
-
-    //
-//
-//    public static void main(String[] args) throws Exception {
-//
-//        caps.setCapability("os", "Windows");
-//        caps.setCapability("os_version", "10");
-//        caps.setCapability("browser", "Chrome");
-//        caps.setCapability("browser_version", "80");
-//
-//        caps.setCapability("name", "mariamunteanu1's First Test");
-//
-//        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-//        driver.get("http://www.google.com");
-//        WebElement element = driver.findElement(By.name("q"));
-//
-//        element.sendKeys("BrowserStack");
-//        element.submit();
-//
-//        System.out.println(driver.getTitle());
-//        driver.quit();
-//    }
+    
     @Given("The browser is launched")
     public void the_browser_is_launched() throws Throwable {
         DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability("os", "Windows");
-//        caps.setCapability("os_version", "10");
-//        caps.setCapability("browser", "Chrome");
-//        caps.setCapability("browser_version", "80");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "80");
 
-        caps.setCapability("name", "mariamunteanu1's First Test");
-        caps.setCapability("browserName", System.getProperty("browser"));
-        caps.setCapability("browserVersion", System.getProperty("browser_version"));
-        caps.setCapability("os", System.getProperty("os"));
-        caps.setCapability("os_version", System.getProperty("os_version"));
-        caps.setCapability("device", System.getProperty("device"));
-//        caps.setCapability("name", "BStack-[Java] Cucumber-Tests"); // test name
         caps.setCapability("build", "Cucumber-Tests"); // CI/CD job or build name
         driver = new RemoteWebDriver(new URL(URL), caps);
     }
